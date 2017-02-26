aaadbc4e9bc779166b155913273d3272c4d8e48e
dnlcrl <ciriello.daniele@gmail.com>
2017-02-26 01:40:37 +0100

    adds exgres files

--- masterplan_exgres/convertgeojson.py
+++ masterplan_exgres/convertgeojson.py
@@ -6,6 +6,8 @@ import os
 import numpy as np
 from json import encoder
 import math
+import numpy as np
+from itertools import islice
 
 # encoder.FLOAT_REPR = lambda o: format(o, '.6f')
 gjindex = -len('.geojson')
@@ -20,6 +22,13 @@ def remove_zetas(m):
             new_m.append(np.array(i)[:, :2].tolist())
         return new_m
 
+def flatten(container):
+    for i in container:
+        if isinstance(i, (list,tuple)):
+            for j in flatten(i):
+                yield j
+        else:
+            yield i
 
 def parse_geojson(fname):
     print fname
@@ -38,11 +47,11 @@ def parse_geojson(fname):
         props['height'] = z
         props['base_height'] = 0
         props['level'] = 1
-        props['name'] = props['name'] if 'name' in props else ''
+        props['name'] = props['name'] if 'name' in props else 'name'
 
         if 'fill' in props:
             props['color'] = props['fill']
-        elif 'stroke' in props:
+        else:
             props['color'] = 'white'
 
         # del props['stroke-opacity']
@@ -51,11 +60,14 @@ def parse_geojson(fname):
         # del props['styleMapHash']
         # del props['styleUrl']
         return props
+
     for file_number in range(int(math.ceil(len(fc.features)/float(num_feats)))):
+
         newgj = FeatureCollection([])
         print file_number
 
         for f in fc.features[file_number*num_feats:(file_number+1)*num_feats]:
+            print f.geometry.type
             if f.geometry.type == 'GeometryCollection':
                 for p in f.geometry.geometries:
                     newprops = fix_props(f.properties, p['coordinates'][0][0][2])
@@ -68,17 +80,19 @@ def parse_geojson(fname):
                     id_feat += 1
 
             else:
+                z = next(islice(flatten(f.geometry['coordinates']), 2, None), 0)
                 f.properties = fix_props(
-                    f.properties, f.geometry['coordinates'][0][0][2])
+                    f.properties, z)
                 # f.geometry['coordinates'] = remove_zetas(f.geometry['coordinates'])
                 newgj.features.append(f)
 
-        with open('new/' + fname[:gjindex] + fname[gjindex:], 'w') as outfile:
-            json.dump(newgj, outfile) #, separators=(',', ':'))
+        # with open('new/' + fname[:gjindex] + str(file_number) +  fname[gjindex:], 'w') as outfile:
+        with open( fname[:gjindex] + fname[gjindex:], 'w') as outfile:
+            json.dump(newgj, outfile, separators=(',', ':'))
 
 
 def main():
-    for fname in [f for f in os.listdir('.') if ('dismesso' in f or 'obsoleto' in f)]:
+    for fname in [f for f in os.listdir('./old') if '_n' not in f and '.' in f and f.split('.')[1] == 'geojson']:
         parse_geojson(fname)
 
 if __name__ == '__main__':
